5f650666380e6cf8b92488c0dca53baae69a3b12
walnuts1018 <r.juglans.1018@gmail.com>
2026-09-20 21:36:56 +0900

    refactor: remove logger, rabbitmq client, worker, and main application code
    - Deleted logger implementation in encoder/internal/logger/logger.go
    - Removed rabbitmq client code in encoder/internal/rabbitmq/client.go
    - Eliminated worker logic in encoder/internal/worker/worker.go
    - Removed main application entry point in encoder/main.go
    - Deleted GraphQL schema definitions in graphql/schema.graphqls
    - Removed Renovate configuration in renovate.json5
    - Deleted Skaffold configuration files in skaffold.yaml
    
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- apiserver/config/oidc.go
+++
@@ -1,7 +0,0 @@
-package config
-
-type OIDCConfig struct {
-	IntrospectionURL string `env:"OIDC_INTROSPECTION_URL"`
-	ClientID         string `env:"OIDC_CLIENT_ID"`
-	ClientSecret     string `env:"OIDC_CLIENT_SECRET"`
-}
